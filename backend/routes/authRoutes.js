9d3dd6af742832ddd025ff2fca907321827d31d0
Vishal404-error <vishal6559.kqr@gmail.com>
2025-01-06 12:34:49 +0530

    Code Refactoring

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,3 @@
-// const express = require("express");
-// const { login } = require("../controllers/authController");
-// const router = express.Router();
-
-// router.post("/auth/login", login);
-
-// module.exports = router;
-
 
 
 const express = require("express");
